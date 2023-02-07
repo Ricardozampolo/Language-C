be806ca6d2ea0aa965b9e9e19932baaa30c07f30
Ricardo Bertolucci <kakazampolo2002@gmail.com>
2023-02-07 10:04:45 -0300

    create fib

---
+++ Projeto e Análise de Algoritmos II/Semestre_1/Recursividade/Fibonacci.c
@@ -0,0 +1,2 @@
+/*A sequência [0, 1, 1, 2, 3, 5, 8, 13, 21, ...] é conhecida como sequência ou série de Fibonacci, e tem aplicações teóricas e práticas, 
+na medida em que alguns padrões na natureza parecem segui-la.Pode ser obtida através de recorrência*/
