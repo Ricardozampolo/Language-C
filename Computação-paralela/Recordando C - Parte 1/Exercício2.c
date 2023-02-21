dbc2f0c01612fe00ba8b9599aa7c65f39753af27
Ricardo Bertolucci <kakazampolo2002@gmail.com>
2023-02-20 23:04:17 -0300

    update ex2

--- Computação-paralela/Recordando C - Parte 1/Exercício2.c
+++ Computação-paralela/Recordando C - Parte 1/Exercício2.c
@@ -1,16 +1,16 @@
 // 2. Escreva um programa que leia 3 notas de um aluno e a média das notas dos exercícios realizados por ele. 
-// Calcular a média de aproveitamento, usando a fórmula: MA = (N1 + N2*2 + N3*3 + ME)/7.
+// Calcular a média de aproveitamento, usando a fórrmula: MA = (N1 + N2*2 + N3*3 + ME)/7.
 // A partir da média, informar o conceito de acordo com a tabela:
 
 #include <stdio.h>
-void testaNota(int a){
+float testaNota(float* a){
     int i = 0;
     
-    scanf("%d", &a);
+    scanf("%f", a);
     while(i < 1){
-        if(a < 0 || a > 10){
-            printf("O valor da nota é inválido. Digite novamente!\n");
-            scanf("%d",&a);
+        if(*a < 0 || *a > 10){
+            printf("O valor da nota e invalido. Digite novamente!\n");
+            scanf("%f",a);
         }
         else {
             i++;
@@ -20,17 +20,20 @@ void testaNota(int a){
 
 int main(){
     //Variáveis
-    int n1, n2, n3, ME;
+    float n1, n2, n3, ME;
     
     printf("Digite o valor da nota N1 de (0 a 10):\n");
-    testaNota(n1);
-    printf("Digite o valor da nota N2 de (0 a 10):\n");
-    testaNota(n2);
-    printf("Digite o valor da nota N3 de (0 a 10):\n");
-    testaNota(n3);
-    printf("Digite o valor da nota ME de (0 a 10):\n");
-    testaNota(ME);
+    testaNota(&n1);
     
-    int media = (n1 + n2*2 + n3*3 + ME)/7;
-    printf("O valor da média de aproveitamento é: %d\n", media);
+    printf("Digite o valor da nota N2 de (0 a 10):\n");
+    testaNota(&n2);
+    
+    printf("Digite o valor da nota N3 de (0 a 10):\n");
+    testaNota(&n3);
+    
+    printf("Digite o valor da nota ME de (0 a 10):\n");
+    testaNota(&ME);
+    
+    float media = (n1 + n2*2 + n3*3 + ME)/7;
+    printf("O valor da media de aproveitamento e: %.2f\n", media);
 }
